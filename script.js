eb4d0198fd40a12b0b322aff546f249d3528b899
Ashish Katiyar <katiyarashish24@gmail.com>
2021-09-30 19:21:20 +0530

    final

--- script.js
+++ script.js
@@ -22,20 +22,20 @@ function calculateProfitOrLoss(initial, quantity, current) {
 		let loss = (initial - current) * quantity;
 		let lossPercentage = (loss / (initial * quantity)) * 100;
 		outputBox.style.color = 'red';
-		outputBox.innerHTML = `Whoops!! Your loss is ${loss} and loss Percentage is ${lossPercentage}% :(`;
+		outputBox.innerHTML = `! Your loss is ${loss} and loss Percentage is ${lossPercentage}% :(`;
 	}
 	// profit calculation
 	else if (initial < current) {
 		let profit = (current - initial) * quantity;
 		let profitPercentage = (profit / (initial * quantity)) * 100;
 		outputBox.style.color = 'green';
-		outputBox.innerHTML = `Yay!! Your Profit is ${profit.toFixed(
+		outputBox.innerHTML = `Your Profit is ${profit.toFixed(
 			2
 		)} and profit Percentage is ${profitPercentage.toFixed(2)}%`;
 	}
 	// No profit, no loss
 	else {
 		outputBox.style.color = 'white';
-		outputBox.innerHTML = `No pain No gain, and no gain no pain :)`;
+		outputBox.innerHTML = `No loss and No Profit.... :)`;
 	}
 }
